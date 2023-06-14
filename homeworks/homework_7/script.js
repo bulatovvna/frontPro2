1024e824e5bb4c500cfde23050434a9899f1a385
Gyuzal <aidarkhanova.g.03@mail.ru>
2023-06-14 20:32:04 +0200

    homework 7

--- homeworks/homework_7/script.js
+++ homeworks/homework_7/script.js
@@ -1,39 +1,46 @@
 let url = "https://dummyjson.com/products"
 
 fetch(url)
-    .then(res => res.join())
+    .then(res => res.json())
     .then(data => {
         createProduct(data)
     })
 
 
-function createProduct(){
-    for( let elem of array){
-        let product = document.createElement('div')
-        product.className = 'product'
+function createProduct(products){
+    let container = document.querySelector('.container')
+
+    products.forEach(product => {
+
+        let productItem = document.createElement('div')
+        productItem.className = 'product'
 
         let img = document.createElement('img')
-        img.setAttribute('src', elem.thumbnail)
+        img.setAttribute('src', product.thumbnail)
+        productItem.append(img)
 
         let p_title = document.createElement('p')
         p_title.className = 'title'
-        p_title.innerText = elem.title.value
+        p_title.innerText = product.title.value
+        productItem.append(p_title)
 
         let p_price = document.createElement('p')
         p_price.className = 'price'
-        p_price.innerText = elem.price
+        p_price.innerText = product.price
+        productItem.append(p_price)
 
         let rating = document.createElement('div')
         rating.className = 'rating'
 
-        for (let i = 1; i < 5; i++) {
+        for (let i = 1; i <= 5; i++) {
             let star = document.createElement('span')
             star.className = 'fa fa-star'
             star.setAttribute('href', "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css")
             rating.append(star)
         }
+        productItem.append(rating)
 
-        product.append(img, p_title, p_price, rating)
-    }
+        container.append(productItem)
+    })
 }
 
