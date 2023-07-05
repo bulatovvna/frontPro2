d6fc86c975953ba36c6423f9b7d023f5313be981
Gyuzal <aidarkhanova.g.03@mail.ru>
2023-07-05 11:34:51 +0200

    lesson 22

---
+++ lessons/lesson_22/script.js
@@ -0,0 +1,41 @@
+let divRoot = document.querySelector('#root')
+let divElem = document.querySelector('.elem')
+
+// let count = 0
+
+// divRoot.onclick = () => {
+//     console.log(count++);
+// }
+
+// let isColor = true
+
+// divElem.onclick = () => {
+//     divRoot.style.backgroundColor = (isColor) ? 'red' : 'aqua'
+//     isColor = !isColor
+// }
+
+// divRoot.ondblclick = () => {
+//     console.log('double');
+// }
+
+// divRoot.onclick = () => {
+//     console.log('click');
+// }
+
+// ------------------------------------
+// addEventListener
+
+// divRoot.addEventListener('click', function(){
+//     console.log('click');
+// })
+
+// divElem.addEventListener('dblclick', function(){
+//     console.log('dblclick');
+// })
+
+// -----------------------------
+// EVENT
+
+// divElem.addEventListener('click', function(event){
+//     console.log(event.offsetX);
+// })
