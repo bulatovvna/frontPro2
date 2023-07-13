764a4dc5831adba4c92986805a7c251274a53481
Gyuzal <aidarkhanova.g.03@mail.ru>
2023-07-13 14:32:43 +0200

    lesson 1 react

--- react/lesson_1/myapp/src/index.js
+++ react/lesson_1/myapp/src/index.js
@@ -4,9 +4,7 @@ import ReactDOM from 'react-dom/client';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <div>
-    <App/>
-    <Post/>
-    <Title/>
+    <ProductCard/>
   </div>
 );
 
@@ -29,6 +27,44 @@ function Title(){
   return (
     <div>
       <h1>Title</h1>
+    </div>
+  )
+}
+
+function ProductCard(){
+  const rand = Math.random()
+  let myVar;
+  (rand < 0.5) ? myVar = 'less' : myVar = 'more'
+  return (
+    <div>
+      <h2>Товар</h2>
+      <p>Desc</p>
+      <p>100 y.e.</p>
+      <button>Добавить в корзину</button>
+      <p>{new Date().toDateString()}</p>
+      <p>{myVar}</p>
+      {/* <p>Value: {rand}, text: {myVar}</p> */}
+      <ButtonsPanel
+        addText = 'Add to Cart'
+        dropText = 'Remove from Cart'
+        rand={rand}
+      />
+      {
+        (rand < 0.5) ? <p>Value: {rand}, text: {myVar}</p> : null 
+      }
+    </div>
+  )
+}
+
+function ButtonsPanel(props){
+  return (
+    <div>
+      <button>{props.addText}</button>
+      {/* <button>{props.dropText}</button> */}
+      {/* <p>{props.rand}</p> */}
+      {
+        (props.rand < 0.5) ? <button>{props.dropText}</button> : null
+      }
     </div>
   )
 }
